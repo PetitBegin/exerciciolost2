90d84f27c9f3dcf2da1fa13736c9f85251e78476
Ana Mara P. Nunes <20242ewbj0090@belojardim.ifpe>
2026-09-15 20:52:10 -0300

    atualizacao

---
+++ carrodecompras.py
@@ -0,0 +1,7 @@
+#16. Um sistema de login permite no máximo 3 tentativas de senha. Caso o usuário erre todas, o acesso
+#deve ser bloqueado — o programa precisa interromper as tentativas assim que esse limite for atingido.#
+senha = "abacate"
+for i in range (3):
+    tentativa = input("Digite sua senha")
+    if senha == tentativa:
+        print("Você")
---
+++ notasdaturma.py
@@ -0,0 +1,13 @@
+#As notas de uma turma foram registradas assim: 
+#notas = [8.5, 6.0, 9.2, 4.5, 7.0, 5.5, 10.
+#0, 3.8, 6.9, 7.5] A partir delas, monte duas novas listas:
+# uma com os aprovados (nota ≥ 7) e outra
+#com os reprovados.#
+notas:list[float] = [8.5, 6.0, 9.2, 4.5, 7.0, 5.5, 10.0, 3.8, 6.9, 7.5]
+aprovados: list[float] = []
+reprovados:list[float] = []
+
+for i in notas:
+    if i <7:
+        reprovados.append(i)
+        
